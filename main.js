899626e1d257ed0f71c3d2cc826021ffe4aa07b8
Mariano <marianoepherra@gmail.com>
2022-10-05 12:23:55 -0300

    proyecto js

---
+++ main.js
@@ -0,0 +1,10 @@
+const menu__btn = document.querySelector("#icono-menu")
+const nav_menu = document.querySelector("#menu")
+
+
+
+
+menu__btn.addEventListener("click", () => {
+    nav_menu.classList.toggle("nav_menu");
+
+});
